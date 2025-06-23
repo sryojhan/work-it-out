a0bd4e652b9403e68162945430481fdaa442396d
Yojhan <sryojhan@gmail.com>
2025-06-23 02:56:44 +0200

    Keyboard styling and card locked background

--- src/components/ExerciseCard.jsx
+++ src/components/ExerciseCard.jsx
@@ -3,12 +3,12 @@
 function ExerciseCard({name, muscleGroup, locked, setLocked}){
 
 
-    return <div className="exercise-card">
+    return <div className={`exercise-card ${locked && 'locked'}`}>
 
         <h4>{muscleGroup}</h4>
         <h2>{name}</h2>
 
-        <button className={`lock-button ${locked ? '' : ''}`} onClick={setLocked}>
+        <button className='lock-button' onClick={setLocked}>
 
             {
             locked ?
